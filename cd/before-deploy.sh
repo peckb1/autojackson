25c6e44995909aefa1a2e9773938def7f6d35c40
Brian Peck <catbert321@gmail.com>
2017-04-01 19:15:11 -0700

    batch the fast import

--- cd/before-deploy.sh
+++ cd/before-deploy.sh
@@ -1,5 +1,5 @@
   #!/usr/bin/env bash
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
     openssl aes-256-cbc -K $encrypted_844fcf1f452a_key -iv $encrypted_844fcf1f452a_iv -in cd/codesigning.asc.enc -out cd/signingkey.asc -d
-    gpg --fast-import cd/signingkey.asc
+    gpg --fast-import cd/signingkey.asc --batch
 fi

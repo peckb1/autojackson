733ac3f5f0d5ef689c335ca470583abcf8b496f5
Brian Peck <catbert321@gmail.com>
2017-04-01 20:08:19 -0700

    adding logging

--- cd/before-deploy.sh
+++ cd/before-deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
     openssl aes-256-cbc -K $encrypted_844fcf1f452a_key -iv $encrypted_844fcf1f452a_iv -in cd/codesigning.asc.enc -out cd/signingkey.asc -d
-    gpg --fast-import --batch cd/sig-ningkey.asc
+    gpg --fast-import --batch --verbose cd/signingkey.asc
 fi
